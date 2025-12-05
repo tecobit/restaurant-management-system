420f1c4cfd29f642f543bcfee5ddbf132bdcdd2c
Bipana Bam <bipanabam147@gmail.com>
2025-12-05 18:27:12 +0545

    settings updated

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6tip4+za1x5dgdmk)@a7qp&m4#9_%4gn!5_rbyrt2b-cae%=hm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -126,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
